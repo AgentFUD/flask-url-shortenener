a7346fc3ec89baefa3e7dc7831616c52645caba1
AgentFUD <agentfud@gmail.com>
2024-08-09 16:42:41 +0200

    project finished

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # url-shortener-demo
 A Simple URL Shortener written in Flask
+
+## How to run the demo ##
+
+clone the repo
+
+```bash
+bash$ git clone git@github.com:AgentFUD/flask-url-shortenener.git
+```
+
+Create a virtual environment and activate it
+```bash
+bash$ python3 -m venv env
+bash$ source env/bin/activate
+```
+
+Install dependencies
+```bash
+bash$ pip3 install -r requirements.txt
+```
+
+Run the app
+```bash
+bash$ python3 app.py
+```
+
+Navigate to [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
+
+## Debug ##
+You can check the database content with the sqlite3 command line tool
+
+```bash
+sqlite3 short_urls.sqlite 
+SQLite version 3.40.1 2022-12-28 14:03:47
+Enter ".help" for usage hints.
+sqlite> select * from urls;
+1|OooQWA|https://www.metzdowd.com/pipermail/cryptography/2023-March/038128.html
+2|KECo1q|https://stackoverflow.com/questions/61378005/decrypting-aes-256-cbc-cipher-with-a-passcode-built-using-pbkdf2-in-python
+sqlite> 
+```
+Enjoy!
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect
+import random
+import string
+from database import Urls, Session
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['long_url']
+        short_url = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+        
+        new_entry = Urls(
+            short_url = short_url,
+            original_url = original_url
+        )
+
+        with Session() as session:
+            session.add(new_entry)
+            session.commit()
+            return f'Shortened URL: {request.url_root}{short_url}'
+    else:
+        return render_template('home.html')
+
+@app.route("/<short_url>", methods=['GET'])
+def jump(short_url):
+    with Session() as session:
+        entry = session.query(Urls).filter(Urls.short_url == short_url).first()
+    return redirect(entry.original_url)
+
+if __name__ == "__main__":
+    app.run(debug=True)
